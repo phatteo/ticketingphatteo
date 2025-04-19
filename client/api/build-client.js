6c1c739d1ced7d05c4a8081242d94c6878d2c783
phatteo <phat01234595087@gmail.com>
2025-04-19 20:37:51 +0700

    met lam roi 6

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
+import https from 'https';
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
-    // We are on the server
-
+    // Running on server => dùng baseURL nội bộ và bỏ qua xác thực SSL
     return axios.create({
-      baseURL:
-        'https://www.hkhviet.xyz/',
+      baseURL: 'https://www.hkhviet.xyz',  // hoặc domain bạn đang dùng
       headers: req.headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,  // ✅ Bỏ qua xác thực SSL (chỉ dùng khi dev hoặc test)
+      }),
     });
   } else {
-    // We must be on the browser
+    // Running in browser
     return axios.create({
-      baseUrl: '/',
+      baseURL: '/',
     });
   }
 };
