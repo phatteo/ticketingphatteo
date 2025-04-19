cd72963ef91342c8d3c6216110b9098acb9f10ae
phatteo <phat01234595087@gmail.com>
2025-04-19 17:19:14 +0700

    cap nhat img lan cuoi 7

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://www.hkhviet.xyz',
       headers: req.headers,
     });
   } else {
