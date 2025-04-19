c5d1a26fec482e768ce2f0017cf978dc4552678e
phatteo <phat01234595087@gmail.com>
2025-04-19 15:57:09 +0700

    cap nhat img lan cuoi 5

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'www.hkhviet.xyz',
+        'http://www.hkhviet.xyz',
       headers: req.headers,
     });
   } else {
