497d7538d89b3e1a6944e1a69ce0d0bdb1c14d39
phatteo <phat01234595087@gmail.com>
2025-04-19 19:46:44 +0700

    met lam roi

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
+import https from 'https';
 
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
-
     return axios.create({
-      baseURL:
-        'https://www.hkhviet.xyz/',
+      baseURL: 'https://www.hkhviet.xyz/',
       headers: req.headers,
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false, // ⛔ BỎ QUA kiểm tra chứng chỉ SSL
+      }),
     });
   } else {
     // We must be on the browser
     return axios.create({
-      baseUrl: '/',
+      baseURL: '/', // lưu ý: phải viết đúng là baseURL (chữ U viết hoa)
     });
   }
 };
