c69b1dda2cc33fc9db5b0ffa7b724f03a2f7ed0c
phatteo <phat01234595087@gmail.com>
2025-04-20 23:17:41 +0700

    met lam roi 25

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
@@ -18,8 +18,9 @@ app.use(currentUser);
 
 app.use(createChargeRouter);
 
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
+// ✅ Sửa lỗi tại đây
+app.all('*', async (req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError());
 });
 
 app.use(errorHandler);
